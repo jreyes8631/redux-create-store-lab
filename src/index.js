c8b3b897c4b54fbe9b0a9d5bedd3c7a12f7ff3da
jreyes8631 <jreyes8631@gmail.com>
2021-02-28 17:03:58 -0500

    commit tree cd7421f980b4dc68d7d2aba43f201d22b5de0ef5

--- src/index.js
+++ src/index.js
@@ -17,6 +17,23 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
-
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
